a06e1301e60ac0588113d2fdd33c8cf57457ebc0
Salma Mamdoh <52897508+Salma-Mamdoh@users.noreply.github.com>
2023-12-10 17:04:55 +0200

    push

---
+++ src/AG_Scheduler.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class AG_Scheduler {
+    
+    public void runAGScheduler(List<Process> processList){
+        System.out.println("hallo");
+    }
+}
---
+++ src/Process.java
@@ -0,0 +1,48 @@
+public class Process {
+    private String Name;
+    private String color;
+    private int arrival_time;
+    private int burst_time;
+    private int priority_number;
+    private int quantum_time;
+    private int contextSwitching_time;
+    private int AG_Factor;
+
+    public Process(String Name, String color, int arrival_time, int burst_time, int priority_number, int quantumTime,int contextSwitching_time) {
+        this.Name=Name;
+        this.color=color;
+        this.arrival_time=arrival_time;
+        this.burst_time=burst_time;
+        this.priority_number=priority_number;
+        this.quantum_time = quantumTime;
+        this.contextSwitching_time=contextSwitching_time;
+        this.AG_Factor = -1;
+    }
+
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getAG_Factor() {
+        return AG_Factor;
+    }
+    public int getArrival_time() {
+        return arrival_time;
+    }
+
+    public int getBurst_time() {
+        return burst_time;
+    }
+
+    public int getPriority_number() {
+        return priority_number;
+    }
+    public int getQuantum_time() {
+        return quantum_time;
+    }
+}
