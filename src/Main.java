670d93f9ac1d074484234e96a8204cb49e0bb70d
Mohamed Wael <101468340+Mo-Wael@users.noreply.github.com>
2023-12-15 14:54:49 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,25 @@ import java.util.*;
 
 public class Main {
         public static void main(String[] args) {
-                ArrayList<Process> test = new ArrayList<>();
-                test.add(new Process(
-                                "P1", "ss", 0, 17, 4,
-                                4, 20));
-                test.add(new Process(
-                                "P2", "ss", 0, 3, 1,
-                                4, 17));
-                test.add(new Process(
-                                "P3", "ss", 3, 6, 2,
-                                4, 16));
-                test.add(new Process(
-                                "P4", "ss", 4, 10, 7,
-                                4, 43));
-                test.add(new Process(
-                                "P5", "ss", 29, 4, 3,
-                                4, 43));
-                PriorityScheduling aa = new PriorityScheduling();
-                aa.runPriorityScheduler(test);
+                View view = new View();
+                view.view();
+                // ArrayList<Process> test = new ArrayList<>();
+                // test.add(new Process(
+                //                 "P1", "ss", 0, 17, 4,
+                //                 4, 20));
+                // test.add(new Process(
+                //                 "P2", "ss", 0, 3, 1,
+                //                 4, 17));
+                // test.add(new Process(
+                //                 "P3", "ss", 3, 6, 2,
+                //                 4, 16));
+                // test.add(new Process(
+                //                 "P4", "ss", 4, 10, 7,
+                //                 4, 43));
+                // test.add(new Process(
+                //                 "P5", "ss", 29, 4, 3,
+                //                 4, 43));
+                // PriorityScheduling aa = new PriorityScheduling();
+                // aa.runPriorityScheduler(test);
         }
 }
